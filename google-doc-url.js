d9419214bfa901a2b23d6c1aad3ad167a164da11
Ahmed Bham <ahmed.bham@gmail.com>
2021-12-15 06:12:49 -0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,5 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/12pp-6NpBuNKUuoR7_er-dHJDWw4XqTqiM-uyGnEqiWo/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = '977d35a0749b34fe9a3f666c5e332032e7b6619a';
+// var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
